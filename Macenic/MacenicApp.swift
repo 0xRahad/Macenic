ecefebc3331d6a10fa4d754a24fb61c002e23ccd
Md Rahadul Islam <apkrahad@gmail.com>
2026-05-07 00:16:22 +0600

    Initial Commit

---
+++ Macenic/MacenicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacenicApp.swift
+//  Macenic
+//
+//  Created by Rahad on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MacenicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
